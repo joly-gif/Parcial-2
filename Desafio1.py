ee7fed79e7b259ce45cef94d5e4ff27bca867165
joly-gif <maria.joly.oliveira@escola.pr.gov.br>
2026-03-31 07:45:22 -0300

    Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,6 @@
 # Pega o primeiro número já como decimal
-n1 = float(input("Número 1: "))
+n1 = float(input('Número 1:'))
 # Pega o segundo número já como decimal
-n2 = float(input("Número 2: "))
+n2 = float(input('Número 2:'))
 # Soma os dois e já mostra o resultado direto na tela
-print("A soma é:", n1 + n2)
+print('A somatória é:', n1 + n2)
